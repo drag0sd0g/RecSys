dab72b4715e45406f95367e0915020fc64574e1b
Dragos Dogaru <dragosdog@gmail.com>
2011-07-05 23:41:24 +0100

    1st

---
+++ src/my/recsys/UserWeightComparator.java
@@ -0,0 +1,15 @@
+package my.recsys;
+
+
+
+
+import java.util.Comparator;
+
+public class UserWeightComparator implements Comparator<UserAndWeight> {
+
+	@Override
+	public int compare(UserAndWeight u1, UserAndWeight u2) {
+		return u2.compareTo(u1);
+	}
+
+}
